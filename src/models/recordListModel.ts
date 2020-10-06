93161b39666804fd9b545a333d78b52ea6df2f15
htz-html <1006676722@qq.com>
2020-10-06 09:09:33 +0800

    重新封装了recordListModel

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -1,14 +1,16 @@
 const localStorageKeyName = 'recordList'
 const recordListModel = {
+    data:[] as RecordItem[],//as表示我断言这是一个RecordItem数组。就不报错了
     clone(data: RecordItem[] | RecordItem){
         return JSON.parse( JSON.stringify(data));
     },
     fetch(){
         // return JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]'); 给fetch的返回值一个类型
-        return JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+        this.data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+        return this.data
     },
-    save(data: RecordItem[]){
-        window.localStorage.setItem(localStorageKeyName, JSON.stringify(data))
+    save(){
+        window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data))
     }
 }
 export default recordListModel;
