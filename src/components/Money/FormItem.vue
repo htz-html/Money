f26d7ab7a2e59220539d24f8fa3f0715a26ad726
htz-html <1006676722@qq.com>
2020-10-06 08:58:48 +0800

    去掉了FormItem中的@watch.修改了ID生成器保存至本地

--- src/components/Money/FormItem.vue
+++ src/components/Money/FormItem.vue
@@ -19,7 +19,7 @@ export default class FormItem extends Vue {
   @Prop({default:''}) readonly value!: string;
   @Prop({required:true}) fieldName!: string;
   @Prop(String) placeholder?: string;
-  @Watch('value')
+  
   onValueChanged(value:string){
     this.$emit("update:value", value)
   }
--- src/lib/createId.ts
+++ src/lib/createId.ts
@@ -2,7 +2,8 @@ let id = parseInt(window.localStorage.getItem('_idMax') || '0') || 0;
 
 function createId(){
     id++
-    return id
+    window.localStorage.setItem('_idMax',id.toString())
+    return id;
 }
 
 export default createId
