4520f7a40c0304faf6ea0e6fe2e044d248d37b8e
htz-html <1006676722@qq.com>
2020-10-12 20:29:00 +0800

    完成统计页面

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -3,9 +3,9 @@
   <Layout>
     <!-- <Types class-prefix="statistics" :value="type" v-on:update:value="type = $event" /> -->
     <Tabs class-prefix="type" :data-source="typeList" :value.sync="type"/>
-    <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
-      <div class="content" v-for="group in result" :key="group.title">
-        <h3>{{beautify(group.title)}}</h3>
+    <!-- <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/> -->
+      <div class="content" v-for="(group,index) in groupedList" :key="index">
+        <h3>{{beautify(group.title)}} <span>{{group.total}}</span></h3>
         <div class="detail" v-for="item in group.items" :key="item.id">
           <span>{{tagString(item.tags)}}</span>
           <span class="use">{{item.notes}}</span>
@@ -19,11 +19,12 @@
 import Tabs from "@/components/Tabs.vue";
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
-import intervalList from '@/constants/intervalList.ts'
+// import intervalList from '@/constants/intervalList.ts'
 import recordTypeList from '@/constants/recordTypeList.ts'
 import recordStore from '../store/recordStore';
 import store from '@/store';
 import dayjs from 'dayjs'
+import clone from '@/lib/clone';
 
 const oneDay = 86400*1000
 @Component({
@@ -32,8 +33,8 @@ const oneDay = 86400*1000
 })
 export default class Statistics extends Vue {
   type: string = "-";
-  interval: string = "day";
-  intervalList= intervalList;
+  // interval: string = "day";
+  // intervalList= intervalList;
   typeList= recordTypeList;
   
   beautify(string: string){
@@ -50,9 +51,6 @@ export default class Statistics extends Vue {
     } else{
       return day.format('YYYY年M月D日')
     }
-    
-
-        
   }
   tagString(tags:Tag[]){
     return tags.length===0? "无": tags[0].name
@@ -60,16 +58,28 @@ export default class Statistics extends Vue {
   get recordList() {
     return (this.$store.state as RootState).recordList;  //要在这里断言一下，说明一下store.state类型，不然就是any，导致recordList没有类型（早成的原因是ts和vue不配合）
   }
-  get result(){
+  get groupedList(){
     const {recordList} = this;
-    type HashTableValue = {title: string, items: RecordItem[]}
-    const hashTable:{[key:string]:HashTableValue} = {};  //声明hashTable的类型，记住，如何声明一个空对象的类型
-    for(let i=0;i<recordList.length;i++){
-      const [date, time] = recordList[i].createdAt!.split('T')
-      hashTable[date] = hashTable[date] || {title:date, items:[] }  //等于他自己，如果是个空的，就等于一个空数组
-      hashTable[date].items.push(recordList[i]);
+    if(recordList.length === 0) {return []}
+    //[ {title:xxx,items:{}}]
+    const newList = clone(recordList)
+    .filter(r=>r.type === this.type)
+    .sort((a,b)=> dayjs(b.createdAt).valueOf()- dayjs(a.createdAt).valueOf())  //sort:排序，使用createdAt时间来排序。但是有的地方可能不需要这样的排序，所以我们深克隆了一下
+    type Result = {title:string, total?:number, items:RecordItem[]}[]
+    const result:Result = [{title:dayjs(newList[0].createdAt).format('YYYY-MM-DD'),items: [newList[0]]}]
+    for(let i=1;i<newList.length-1;i++){
+      const current = newList[i];
+      const last = result[result.length -1]
+      if((dayjs(last.title)).isSame(dayjs(current.createdAt),'day')) {
+        last.items.push(current)
+      } else {
+        result.push({title:dayjs(current.createdAt).format('YYYY-MM-DD'),items: [current]})
+      }
     }
-    return hashTable
+    result.map(group=>{  //区分map(有返回值的forEach)和forEach（没有返回值的map）
+      group.total = group.items.reduce((sum, item)=>sum + item.amount, 0);
+    })
+    return result
   }
 
   created() {
@@ -91,7 +101,9 @@ export default class Statistics extends Vue {
   > h3{
     min-height: 40px;
     line-height: 40px;
-    padding-left: 15px;
+    padding: 0 15px;
+    display: flex;
+    justify-content: space-between;
   }
   > .detail {
     position: relative;
